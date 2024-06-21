61d2f33676620e7c791b6998bf4a51e85075a4cc
Adrián Maza <adri_mv1@hotmail.com>
2024-06-21 13:23:59 +0200

    05-route-params

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PokemonsPage from "./pages/PokemonsPage";
+import PokemonPage from "./pages/PokemonPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/pokemons" element={<PokemonsPage />} />
+      <Route path="/pokemons/:id" element={<PokemonPage />} />
     </Routes>
   );
 }
