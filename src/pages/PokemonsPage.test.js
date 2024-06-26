afd0e6959c636734f2357a00232d3d1ad635ef19
Adrián Maza <adri_mv1@hotmail.com>
2024-06-26 10:03:02 +0200

    react-testing-library & jest configuration

---
+++ src/pages/PokemonsPage.test.js
@@ -0,0 +1,9 @@
+// import { render } from "@testing-library/react";
+import PokemonsPage from "./PokemonsPage";
+
+describe(PokemonsPage, () => {
+  it("simple test", () => {
+    // const {} = render(<PokemonsPage/>)
+    expect(1).toEqual(1);
+  })
+})
