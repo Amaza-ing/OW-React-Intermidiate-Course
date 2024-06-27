256ee12d1b18de32c88f2d2242551426c5dc360d
Adrián Maza <adri_mv1@hotmail.com>
2024-06-27 10:32:14 +0200

    mock API call from context

--- src/api.js
+++
@@ -1,4 +0,0 @@
-export const fetchPokemon = async (index) => {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${index}`);
-  return response.json();  
-};
--- src/pages/PokemonPage.test.js
+++ src/pages/PokemonPage.test.js
@@ -1,14 +1,11 @@
 import { render, screen } from "@testing-library/react";
-import * as api from "../api";
-import { PokemonProviderWrapper } from "../context/pokemon.context";
+import { PokemonContext } from "../context/pokemon.context";
 import PokemonPage from "./PokemonPage";
 import { MemoryRouter } from "react-router-dom";
 
-jest.mock("../api");
-
 describe("Pokemon Page", () => {
   it("Should get bulbasaur", async () => {
-    api.fetchPokemon.mockResolvedValue({
+    const fetchPokemon = (id) => ({
       name: "bulbasaur",
       sprites: {
         front_default: "picture.jpg",
@@ -26,11 +23,11 @@ describe("Pokemon Page", () => {
       ],
     });
 
-    const { getByTestId } = render(
+    render(
       <MemoryRouter>
-        <PokemonProviderWrapper>
+        <PokemonContext.Provider value={{ fetchPokemon }}>
           <PokemonPage />
-        </PokemonProviderWrapper>
+        </PokemonContext.Provider>
       </MemoryRouter>
     );
 
